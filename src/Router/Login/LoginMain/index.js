bc7366f428b4d7e60245a6049d02fbb772dbfdfa
mindy <86297534@naver.com>
2020-04-20 16:02:12 +0900

    로그인페이지 button -> checkbox로 변경

--- src/Router/Login/LoginMain/index.js
+++ src/Router/Login/LoginMain/index.js
@@ -149,18 +149,18 @@ return(
 
                         <label className="title_label" htmlFor="agreement1">서비스 이용약관</label>
                         <textarea id="agreement3" readOnly>
-■ 수집하는 개인정보 항목
-회사는 회원가입, 상담, 서비스 신청 등등을 위해 아래와 같은 개인정보를 수집하고 있습니다.
-ο 수집항목 : 이름 , 생년월일 , 성별 , 로그인ID , 비밀번호 , 비밀번호 질문과 답변 , 자택 전화번호 , 자택 주소 , 휴대전화번호 , 이메일 , 직업 , 회사명 , 부서 , 직책 , 회사전화번호 , 취미 , 결혼여부 , 기념일 , 법정대리인정보 , 서비스 이용기록 , 접속 로그 , 접속 IP 정보 , 결제기록
-ο 개인정보 수집방법 : 홈페이지(회원가입) , 서면양식
-■ 개인정보의 수집 및 이용목적
-회사는 수집한 개인정보를 다음의 목적을 위해 활용합니다.
-ο 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산 콘텐츠 제공 , 구매 및 요금 결제 , 물품배송 또는 청구지 등 발송
-ο 회원 관리
-회원제 서비스 이용에 따른 본인확인 , 개인 식별   , 연령확인 , 만14세 미만 아동 개인정보 수집 시 법정 대리인 동의여부 확인 , 고지사항 전달 ο 마케팅 및 광고에 활용
-접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계
-■ 개인정보의 보유 및 이용기간
-회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
+                            ■ 수집하는 개인정보 항목
+                            회사는 회원가입, 상담, 서비스 신청 등등을 위해 아래와 같은 개인정보를 수집하고 있습니다.
+                            ο 수집항목 : 이름 , 생년월일 , 성별 , 로그인ID , 비밀번호 , 비밀번호 질문과 답변 , 자택 전화번호 , 자택 주소 , 휴대전화번호 , 이메일 , 직업 , 회사명 , 부서 , 직책 , 회사전화번호 , 취미 , 결혼여부 , 기념일 , 법정대리인정보 , 서비스 이용기록 , 접속 로그 , 접속 IP 정보 , 결제기록
+                            ο 개인정보 수집방법 : 홈페이지(회원가입) , 서면양식
+                            ■ 개인정보의 수집 및 이용목적
+                            회사는 수집한 개인정보를 다음의 목적을 위해 활용합니다.
+                            ο 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산 콘텐츠 제공 , 구매 및 요금 결제 , 물품배송 또는 청구지 등 발송
+                            ο 회원 관리
+                            회원제 서비스 이용에 따른 본인확인 , 개인 식별   , 연령확인 , 만14세 미만 아동 개인정보 수집 시 법정 대리인 동의여부 확인 , 고지사항 전달 ο 마케팅 및 광고에 활용
+                            접속 빈도 파악 또는 회원의 서비스 이용에 대한 통계
+                            ■ 개인정보의 보유 및 이용기간
+                            회사는 개인정보 수집 및 이용목적이 달성된 후에는 예외 없이 해당 정보를 지체 없이 파기합니다.
                         </textarea>
                         <p className="radio_box">
                             <input type="radio" id="confirm3_y" name="confirm3"/>
@@ -196,16 +196,30 @@ return(
                         <h1 style={{marginTop:"-57px", fontWeight:"300"}}>운영하시는 <strong>기관의 종류</strong>를 선택해 주세요.</h1>
                         <p style={{fontSize:"22px", color:"#6abcc6", marginBottom:"45px"}}>*중복선택이 가능합니다.</p>
                     </div>
-                    <div>
+                    <div class="checkbox">
                         {/* 이미지넣기 */}
-                        <button type="button" class="choose num00 mg_le_0" value="0" style={{width:"311px", height:"460px"}}></button>
-                        <button type="button" class="choose num01" value="1"></button>
-                        <button type="button" class="choose num02" value="2"></button>
-                        <button type="button" class="choose num03" value="3"></button>
+                        <ul>
+                            <li>
+                                <input type="checkbox" id="num00"/>
+                                <label for="num00" class="choose num00 mg_le_0"></label>
+                            </li>
+                            <li>
+                                <input type="checkbox" id="num01"/>
+                                <label for="num01" class="choose num01"></label>
+                            </li>
+                            <li>
+                                <input type="checkbox" id="num02"/>
+                                <label for="num02" class="choose num02"></label>
+                            </li>
+                            <li>
+                                <input type="checkbox" id="num03"/>
+                                <label for="num03" class="choose num03"></label>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <p className="btn_box_mb3">
-                    <button className="btn_next" style={{position:"absolute", top:"120%", left:"45%"}} onClick={()=>openJoinForm2()}>다음으로</button>
+                    <button className="btn_next" onClick={()=>openJoinForm2()}>다음으로</button>
                 </p>
             </div>
 
